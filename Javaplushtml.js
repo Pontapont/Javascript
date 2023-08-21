707b27074c69eb247046200c9cebb18129f2db5f
Pontapont <111230403+Pontapont@users.noreply.github.com>
2023-08-21 16:55:53 +0200

    Update Javaplushtml.js

--- Javaplushtml.js
+++ Javaplushtml.js
@@ -1,6 +1,7 @@
 // Event listener that waits for click on button. After which it executes onclick parameter
 <button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
 <button onclick="this.innerHTML = Date()">The time is?</button>
+document.querySelector("#header").innerHTML = "Hallo";
 
 Common HTML Events
 
@@ -13,4 +14,16 @@ Event 	Description
 -  onkeydown 	The user pushes a keyboard key
 -  onload 	The browser has finished loading the page
 
+// The HTML DOM document object is the owner of all other objects in your web page.
+The HTML DOM Document Object
+The document object represents your web page.
+If you want to access any element in an HTML page, you always start with accessing the document object.
+Below are some examples of how you can use the document object to access and manipulate HTML.
 
+// Select all <p> tags within <header> and log second <p> tag
+const myT = document.getElementById("header");
+const myB = myT.getElementsByTagName("p");
+console.log(myB[2].innerHTML)
+
+// Select all <p> that have a "intro" class
+let a = document.querySelectorAll("p.intro");
