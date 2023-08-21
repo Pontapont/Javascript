0d3c5394a65f47856c5661b75a089a09943e7aea
Pontapont <111230403+Pontapont@users.noreply.github.com>
2023-08-21 15:36:31 +0200

    Update handy_2.js

--- handy_2.js
+++ handy_2.js
@@ -84,3 +84,38 @@ function switchOfStuff(val) {
 // Math operators
 Math.round(o + 0.4));
 console.log(Math.sqrt(9))
+
+// Convert data type
+Number()
+String()
+parseFloat() parse string and return floating point number
+parseInt() parse string and return integer
+toFixed()
+
+// Arrow functions
+const multiply = (num1, num2) => {
+  const result = num1 * num2
+  return result
+}
+
+const saySomething = () => {
+  console.log("Sayyyy somethinnggggg");
+}
+
+
+// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. 
+// They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
+
+// In JavaScript, you can escape a quote from considering it as an end of string quote by placing a backslash (\) in front of the quote.
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+
+// Escape Sequences:
+
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
