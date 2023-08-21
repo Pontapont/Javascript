589417c73d149e739072e28429e671be6e767d48
Pontapont <111230403+Pontapont@users.noreply.github.com>
2023-08-21 14:18:19 +0200

    Update handy_2.js

--- handy_2.js
+++ handy_2.js
@@ -30,3 +30,46 @@ myObject["omr"] = 28;
 if (!("omar" in myObject)) {
     console.log(false);
 }
+
+// Turn array into a string
+const testArr = [1, 2, 3, 4, 5];
+const myVar = JSON.stringify(testArr);
+// myVar = "[1, 2, 3, 4, 5]"
+
+// The equality operator '==' makes no difference between a Integer and string value. 
+// that means "8" == 8.
+const x = 8;
+const z = "8";
+if (x == z) {
+  console.log(true);
+} else {console.log(false)}
+
+// &&
+if (val >= 25 && val <= 50) {
+    return "Yes";
+}
+
+// The logical or operator (||) returns true if either of the operands is true. Otherwise, it returns false.
+
+// Switch statement
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break
+    case "b":
+      answer = "bird";
+      break
+    case "c":
+      answer = "cat";
+      break
+    default:
+      answer = "stuff";
+      break
+  }
+
+  // Only change code above this line
+  return answer;
+}
