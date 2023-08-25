cf1a26e995ff5df1f43ed0fadc80fe8ac4109140
Pontapont <111230403+Pontapont@users.noreply.github.com>
2023-08-25 15:38:49 +0200

    Update handy_3.js

--- handy_3.js
+++ handy_3.js
@@ -6,3 +6,17 @@ console.log(parseInt(min, 2));
 Math.min(4, 2, 10); // returns 2
 let numbers = [2, 55, 9, 1];
 Math.min(...numbers) // returns 1. The ... is not a joke
+
+// Map function. Loops through array and applies function in map parameter to every element
+let myarr = [1, 5, 9];
+function mathStuff(int) {
+  console.log(int*int);
+};
+myarr.map(mathStuff);
+
+// Make array with "1", "5" and "3" as elements with split(). Use every element inside testMe() function with map().
+// map() returns array
+// Use every element in array created by map function with forEach(). parameter value "num" is current array element being used.
+"153".split("").map(testMe).forEach( num => {
+  sum += num;
+});
